e188f77559d9b626984b3c1e6615afdba28e42d7
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-07 20:25:29 -0400

    feat: add DataSeeder class for database initialization with test data

---
+++ src/main/java/com/veterinarynux/veterinary_api/bootstrap/DataSeeder.java
@@ -0,0 +1,8 @@
+package com.veterinarynux.veterinary_api.bootstrap;
+
+public class DataSeeder {
+  // This class is responsible for seeding the database with initial data.
+  // It will create instances of VeterinarianDetails and ClientDetails
+  // and save them to the database.
+  // The data will be used for testing and development purposes.
+}
