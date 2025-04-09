6118ee2bd46522cf831e3c3200ba1202cf576082
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-09 19:51:43 -0400

    refactor(AppointmentRepository): remove unused findByDateBetween method

--- src/main/java/com/veterinarynux/veterinary_api/repository/AppointmentRepository.java
+++ src/main/java/com/veterinarynux/veterinary_api/repository/AppointmentRepository.java
@@ -1,6 +1,5 @@
 package com.veterinarynux.veterinary_api.repository;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,8 +8,6 @@ import com.veterinarynux.veterinary_api.model.Appointment;
 import com.veterinarynux.veterinary_api.model.enums.AppointmentStatus;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-
-  List<Appointment> findByDateBetween(LocalDate startDate, LocalDate endDate);
 
   List<Appointment> findByStatus(AppointmentStatus status);
 }
