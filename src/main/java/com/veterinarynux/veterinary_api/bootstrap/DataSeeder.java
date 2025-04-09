35661d81b8cebefc949c61233aaf0746859801b5
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-09 09:25:11 -0400

    feat: implement DataSeeder for initializing database with sample user data

--- src/main/java/com/veterinarynux/veterinary_api/bootstrap/DataSeeder.java
+++ src/main/java/com/veterinarynux/veterinary_api/bootstrap/DataSeeder.java
@@ -1,8 +1,43 @@
 package com.veterinarynux.veterinary_api.bootstrap;
 
-public class DataSeeder {
-  // This class is responsible for seeding the database with initial data.
-  // It will create instances of VeterinarianDetails and ClientDetails
-  // and save them to the database.
-  // The data will be used for testing and development purposes.
+import java.util.Set;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.veterinarynux.veterinary_api.service.UserService;
+import com.veterinarynux.veterinary_api.model.ClientDetails;
+import com.veterinarynux.veterinary_api.model.Role;
+import com.veterinarynux.veterinary_api.model.User;
+import com.veterinarynux.veterinary_api.model.enums.*;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class DataSeeder implements CommandLineRunner {
+  private final UserService userService;
+
+  @Override
+  public void run(String... args) throws Exception {
+    User client = User.builder()
+        .name("Cristian")
+        .email("admin@example.com")
+        .username("admin")
+        .password("admin")
+        .phone("123456789")
+        .roles(Set.of(Role.builder()
+            .name(RoleEnum.CLIENT)
+            .build()))
+        .address("123 Main St")
+        .build();
+
+    ClientDetails details = ClientDetails.builder()
+        .user(client)
+        .build();
+
+    client.setClienteDetails(details);
+    userService.registerUser(client);
+  }
+
 }
