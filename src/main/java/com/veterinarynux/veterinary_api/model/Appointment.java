cfdbb05b6d134d6a1bf52e82e96cd28bbfab39b8
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-09 14:15:49 -0400

    feat(appointment): add description field to Appointment model

--- src/main/java/com/veterinarynux/veterinary_api/model/Appointment.java
+++ src/main/java/com/veterinarynux/veterinary_api/model/Appointment.java
@@ -20,6 +20,8 @@ public class Appointment {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(name = "description", nullable = false)
+  private String description;
   @ManyToOne
   @JoinColumn(name = "client_id", nullable = false)
   private ClientDetails clientDetails;
