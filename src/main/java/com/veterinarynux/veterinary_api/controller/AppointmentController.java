4cda25c2c172e1224ca7c75e95755130c966ca6b
manuujrodcruz <mrmanueljoserodriguezcruz@gmail.com>
2025-04-09 19:59:39 -0400

    WIP: loading branch master updates on appointment

--- src/main/java/com/veterinarynux/veterinary_api/controller/AppointmentController.java
+++ src/main/java/com/veterinarynux/veterinary_api/controller/AppointmentController.java
@@ -1,14 +1,37 @@
 package com.veterinarynux.veterinary_api.controller;
 
+import com.veterinarynux.veterinary_api.model.Appointment;
+import com.veterinarynux.veterinary_api.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.veterinarynux.veterinary_api.service.AppointmentService;
 
 import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/appointments")
-@RequiredArgsConstructor
+@Controller
 public class AppointmentController {
 
   private final AppointmentService appointmentService;
+
+  @Autowired
+  public AppointmentController(AppointmentService appointmentService) {
+    this.appointmentService = appointmentService;
+  }
+
+  @GetMapping
+  public String createAppointment() {
+    return "pages/createAppointment";
+  }
+
+  @PostMapping
+  public String createAppointment(@RequestBody Appointment appointment) {
+    appointmentService.createAppointment(appointment);
+    return "pages/createAppointment";
+  }
 
 }
