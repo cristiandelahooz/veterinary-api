dc815552246ed002d8033842c4c35db17e6945f7
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-09 14:15:28 -0400

    feat(data-seeder): add veterinarian user and create appointment during data seeding

--- src/main/java/com/veterinarynux/veterinary_api/bootstrap/DataSeeder.java
+++ src/main/java/com/veterinarynux/veterinary_api/bootstrap/DataSeeder.java
@@ -1,14 +1,18 @@
 package com.veterinarynux.veterinary_api.bootstrap;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.veterinarynux.veterinary_api.service.AppointmentService;
 import com.veterinarynux.veterinary_api.service.UserService;
+import com.veterinarynux.veterinary_api.model.Appointment;
 import com.veterinarynux.veterinary_api.model.ClientDetails;
 import com.veterinarynux.veterinary_api.model.Role;
 import com.veterinarynux.veterinary_api.model.User;
+import com.veterinarynux.veterinary_api.model.VeterinarianDetails;
 import com.veterinarynux.veterinary_api.model.enums.*;
 
 import lombok.RequiredArgsConstructor;
@@ -17,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class DataSeeder implements CommandLineRunner {
   private final UserService userService;
+  private final AppointmentService appointmentService;
 
   @Override
   public void run(String... args) throws Exception {
@@ -32,12 +37,41 @@ public class DataSeeder implements CommandLineRunner {
         .address("123 Main St")
         .build();
 
+    User veterinarian = User.builder()
+        .name("Dr. Smith")
+        .email("smith@gmail.com")
+        .username("drsmith")
+        .password("password")
+        .phone("987654321")
+        .roles(Set.of(Role.builder()
+            .name(RoleEnum.VETERINARIAN)
+            .build()))
+        .address("456 Elm St")
+        .build();
+
     ClientDetails details = ClientDetails.builder()
         .user(client)
         .build();
 
-    client.setClienteDetails(details);
-    userService.registerUser(client);
-  }
+    VeterinarianDetails veterinarianDetails = VeterinarianDetails.builder()
+        .user(veterinarian)
+        .specialty(Specialty.EMERGENCY_AND_CRITICAL_CARE)
+        .build();
 
+    client.setClienteDetails(details);
+    veterinarian.setVeterinarianDetails(veterinarianDetails);
+    userService.registerUser(client);
+    userService.registerUser(veterinarian);
+
+    Appointment appointment = Appointment.builder()
+        .description("Annual check-up")
+        .date(LocalDateTime.now())
+        .status(AppointmentStatus.SCHEDULED)
+        .type(AppointmentType.CHECKUP)
+        .clientDetails(details)
+        .veterinarianDetails(veterinarianDetails)
+        .build();
+
+    appointmentService.createAppointment(appointment);
+  }
 }
