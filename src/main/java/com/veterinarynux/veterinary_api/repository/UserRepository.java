d532024c23dc61caf97feb84c3ad18bdede883c1
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-09 09:26:53 -0400

    feat: add findByUsername method to UserRepository for user retrieval by username

--- src/main/java/com/veterinarynux/veterinary_api/repository/UserRepository.java
+++ src/main/java/com/veterinarynux/veterinary_api/repository/UserRepository.java
@@ -7,6 +7,8 @@ import com.veterinarynux.veterinary_api.model.User;
 public interface UserRepository extends JpaRepository<User, Long> {
   User findByEmail(String email);
 
+  User findByUsername(String username);
+
   boolean existsByEmail(String email);
 
   boolean existsByUsername(String username);
